b2b59de191978645bf41e5757135384ced24be86
Mir Muhammad Seyyedghasemi <seyedlgnexus@gmail.com>
2025-01-21 11:20:00 +0330

    Add files via upload

---
+++ Emtehan.py
@@ -0,0 +1,13 @@
+import sys
+import math
+
+
+class food:
+    def __init__(self,food,personel, costumer):
+        self.food = food
+        self.personel = personel
+        self.costumer = costumer
+self.food_list=['bereng':120,'kabab':100,'makaroni':90,'soup':90,'morgh':120,'khoresht gheyme':150,'nooshabe':50,'mast':20,'ab':15]           
+    def adding_food(self, food_name, food_price):
+            adding food = input(self.food_list)
+            
